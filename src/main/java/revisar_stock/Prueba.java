94498baa7e0d4b532de13bb23d50881ce07a656a
WillGM2003 <willgm.2003@gmail.com>
2023-02-25 13:04:06 -0500

    prueba 2

---
+++ src/main/java/revisar_stock/Prueba.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package revisar_stock;
+
+/**
+ *
+ * @author WGM1200
+ */
+public class Prueba {
+    
+}
